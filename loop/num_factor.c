82a3c19f03db4170426e633997ef2a7b6f52436f
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-16 01:49:56 +0600

    Enhance factor finding program with user prompts
    Added prompt for user input and improved output message.

--- loop/num_factor.c
+++ loop/num_factor.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
     int n, i = 1;
+    printf("Enter a number to find all factors: ");
     scanf("%d", &n);
     while(i <= n) {
-        if(n % i == 0) printf("%d ", i);
+        if(n % i == 0) printf("All factors given below:\n %d ", i);
         i++;
     }
     return 0;
