0691f05256886faa9dc7c0f4b16f4db285d04291
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-16 02:06:33 +0600

    Update factorial calculation to use long long type
    Change factorial variable type to long long for larger results.

--- loop/factorial.c
+++ loop/factorial.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
-    int n, i = 1, f = 1;
+    int n, i = 1;
+    long long f = 1;
     scanf("%d", &n);
     while(i <= n) {
         f *= i;
         i++;
     }
-    printf("%d", f);
+    printf("%lld", f);
     return 0;
 }
