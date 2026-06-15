2c3c63ddfa5b72122e912e92c40d786031b0b3e7
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-16 02:11:26 +0600

    Add overflow check for factorial calculation
    Added a check for input greater than 20 to prevent overflow.

--- loop/factorial.c
+++ loop/factorial.c
@@ -2,12 +2,18 @@
 int main() {
     int n, i = 1;
     long long f = 1;
-    printf("Enter a number to calculate factorial of the number: ");
+    printf("Enter a number to calculate factorial: ");
     scanf("%d", &n);
+    if (n > 20) {
+        printf("Factorial exceeds long long range.\n");
+        return 1;
+    }
+    else{
     while(i <= n) {
         f *= i;
         i++;
     }
     printf("The answer is: %lld", f);
+    }
     return 0;
 }
