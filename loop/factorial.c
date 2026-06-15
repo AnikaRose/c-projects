3e872f27842380e0990018b25ee2702ded7160a4
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-16 02:12:32 +0600

    Add negative number check for factorial calculation

--- loop/factorial.c
+++ loop/factorial.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+
 int main() {
     int n, i = 1;
     long long f = 1;
+
     printf("Enter a number to calculate factorial: ");
     scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+        return 1;
+    }
+
     if (n > 20) {
         printf("Factorial exceeds long long range.\n");
         return 1;
     }
-    else{
-    while(i <= n) {
+
+    while (i <= n) {
         f *= i;
         i++;
     }
-    printf("The answer is: %lld", f);
-    }
+
+    printf("The answer is: %lld\n", f);
+
     return 0;
 }
