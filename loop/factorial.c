d8d7aff8a110e048cf8831acb7d5ead7381b6a91
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-16 02:08:46 +0600

    Enhance factorial program with user prompts
    Added user prompt for input and formatted output message.

--- loop/factorial.c
+++ loop/factorial.c
@@ -2,11 +2,12 @@
 int main() {
     int n, i = 1;
     long long f = 1;
+    printf("Enter a number to calculate factorial of the number: ");
     scanf("%d", &n);
     while(i <= n) {
         f *= i;
         i++;
     }
-    printf("%lld", f);
+    printf("The answer is: %lld", f);
     return 0;
 }
