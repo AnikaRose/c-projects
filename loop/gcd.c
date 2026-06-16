c697a1821cb79c0c800e30ab00cb2d76d6a2f216
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-17 01:27:04 +0600

    Enhance GCD program with user prompts
    Added prompt and output message for GCD calculation.

--- loop/gcd.c
+++ loop/gcd.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
     int a, b;
+    printf("Enter two numbers to find GCD: ");
     scanf("%d %d", &a, &b);
     while(a != b) {
         if(a > b) a -= b;
         else b -= a;
     }
-    printf("%d", a);
+    printf("The GCD is: %d", a);
     return 0;
 }
