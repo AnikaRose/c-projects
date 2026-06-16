e82f05045baec8e05a8a882af2f25627828c9133
Anika Rose Rimi <anikaroserimi@gmail.com>
2026-06-17 01:30:46 +0600

    Add user prompt and output message for LCM calculation

--- loop/lcm.c
+++ loop/lcm.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main() {
     int a, b, x, y;
+    printf("Enter two numbers to find LCM: ");
     scanf("%d %d", &a, &b);
     x = a; y = b;
     while(a != b) {
         if(a > b) a -= b;
         else b -= a;
     }
-    printf("%d", (x*y)/a);
+    printf("The LCM is: %d", (x*y)/a);
     return 0;
 }
